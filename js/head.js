2323cdd5601472480f685ba04371a72eb109588f
pikachu832 <76896832+pikachu832@users.noreply.github.com>
2025-01-07 08:56:24 +0200

    Add files via upload

---
+++ js/head.js
@@ -0,0 +1,16 @@
+function toggleForm() {
+    var form = document.getElementById("id01");
+    if (form.style.display === "none") {
+        form.style.display = "block";
+    } else {
+        form.style.display = "none";
+    }
+}
+// function toggleForm() {
+//     var form = document.getElementById("id01");
+//     if (form.style.display === "none") {
+//         form.style.display = "block";
+//     } else {
+//         form.style.display = "none";
+//     }
+// }
